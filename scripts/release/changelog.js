68b4b7d57865261ec7df45b95d0c47e79a4f0b70
HobbyScripter <ramonoltmann@gmail.com>
2026-07-25 18:12:58 +0200

    refactor: improve code formatting and organization in changelog parser

--- scripts/release/changelog.js
+++ scripts/release/changelog.js
@@ -1,10 +1,11 @@
 /**
- * WebRadio Release Infrastructure
- *
- * CHANGELOG parser.
- *
- * @module Release/Changelog
- */
+
+* WebRadio Release Infrastructure
+*
+* CHANGELOG parser.
+*
+* @module Release/Changelog
+  */
 
 'use strict';
 
@@ -12,76 +13,114 @@ const constants = require('./constants');
 const utils = require('./utils');
 
 const {
-    ChangelogError
+ChangelogError
 } = require('./errors');
 
 /**
- * Returns the changelog section for a specific version.
- *
- * @param {string} version
- * @returns {string}
- */
-function getVersion(version) {
 
-    const markdown = utils.readFile(
-        constants.PATHS.CHANGELOG
-    );
-
-    const heading = `## [${version}]`;
-
-    const start = markdown.indexOf(
-        heading
-    );
-
-    if (start === -1) {
-
-        throw new ChangelogError(
-            `Version "${version}" was not found in CHANGELOG.md.`
-        );
-
-    }
-
-    const next = markdown.indexOf(
-        '\n## [',
-        start + heading.length
-    );
-
-    if (next === -1) {
-
-        return markdown
-            .substring(start)
-            .trim();
-
-    }
-
-    return markdown
-        .substring(start, next)
-        .trim();
-
-}
+* Normalizes a version string by removing an optional leading "v".
+*
+* Examples:
+* v1.0.5-rc.4 -> 1.0.5-rc.4
+* 1.0.5-rc.4  -> 1.0.5-rc.4
+*
+* @param {string} version
+* @returns {string}
+  */
+  function normalizeVersion(version) {
+  return String(version).replace(/^v/, '');
+  }
 
 /**
- * Returns true if a version exists.
- *
- * @param {string} version
- * @returns {boolean}
- */
-function hasVersion(version) {
 
-    const markdown = utils.readFile(
-        constants.PATHS.CHANGELOG
-    );
+* Returns all supported CHANGELOG headings for a version.
+*
+* Both of the following formats are supported:
+*
+* ## [1.0.5-rc.4]
+* ## [v1.0.5-rc.4]
+*
+* @param {string} version
+* @returns {string[]}
+  */
+  function getHeadings(version) {
+  const normalizedVersion = normalizeVersion(version);
 
-    return markdown.includes(
-        `## [${version}]`
-    );
+  return [
+  `## [${normalizedVersion}]`,
+  `## [v${normalizedVersion}]`
+  ];
+  }
 
-}
+/**
+
+* Returns the changelog section for a specific version.
+*
+* @param {string} version
+* @returns {string}
+  */
+  function getVersion(version) {
+  const markdown = utils.readFile(
+  constants.PATHS.CHANGELOG
+  );
+
+  const headings = getHeadings(version);
+
+  let start = -1;
+  let matchedHeading = null;
+
+  for (const heading of headings) {
+  const index = markdown.indexOf(heading);
+
+  ```
+   if (index !== -1 && (start === -1 || index < start)) {
+       start = index;
+       matchedHeading = heading;
+   }
+  ```
+
+  }
+
+  if (start === -1) {
+  throw new ChangelogError(
+  `Version "${version}" was not found in CHANGELOG.md.`
+  );
+  }
+
+  const next = markdown.indexOf(
+  '\n## [',
+  start + matchedHeading.length
+  );
+
+  if (next === -1) {
+  return markdown
+  .substring(start)
+  .trim();
+  }
+
+  return markdown
+  .substring(start, next)
+  .trim();
+  }
+
+/**
+
+* Returns true if a version exists.
+*
+* @param {string} version
+* @returns {boolean}
+  */
+  function hasVersion(version) {
+  const markdown = utils.readFile(
+  constants.PATHS.CHANGELOG
+  );
+
+  return getHeadings(version).some(
+  heading => markdown.includes(heading)
+  );
+  }
 
 module.exports = Object.freeze({
-
-    getVersion,
-
-    hasVersion
-
+getVersion,
+hasVersion
 });
